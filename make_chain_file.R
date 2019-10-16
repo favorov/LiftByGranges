ee4e4fc271025f7ef447bce9eadf6f169de163df
Veronica Busa <veronica.busa@gmail.com>
2019-10-16 14:40:00 -0400

    Allow for hg38 (instead of just hg19) mapping of transcriptome

--- make_chain_file.R
+++ make_chain_file.R
@@ -7,6 +7,7 @@
 #' @param chrom_suffix The suffix to be appended to all chromosome names created in the chain file
 #' @param verbose Output updates while the function is running. Default FALSE
 #' @param transcript_list a vector of transcript names that represent the most expressed isoform of their respective genes and correspond to gtf annotation names
+#' @param alignment The human genome alignment to be used, either "hg19" or "hg38". Default "hg19"
 #' 
 #' @examples 
 #' \dontrun{
@@ -14,11 +15,11 @@
 #'   "hg19_annotations_ensembl_exome.chain",
 #'   c("ENST00000600805", "ENST00000595082", "ENST00000597230", "ENST00000595005")
 #'   chrom_suffix="exome_muscle",
-#'   verbose=T,)
+#'   verbose=T)
 #' }
 #' 
 
-GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list, chrom_suffix = "exome", verbose=FALSE){
+GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list, chrom_suffix = "exome", verbose=FALSE, alignment="hg19"){
   # first load necessary packages, rtracklayer, plyranges, and TxDb.Hsapiens.UCSC.hg19.knownGene
   if("rtracklayer" %in% gsub("package:", "", search())){
     if(verbose==TRUE){print("rtracklayer is loaded correctly")}
@@ -26,20 +27,28 @@ GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list, 
   if("plyranges" %in% gsub("package:", "", search())){
     if(verbose==TRUE){print("plyranges is loaded correctly")}
   } else {print("please install and/or load plyranges")}
-  if("TxDb.Hsapiens.UCSC.hg19.knownGene" %in% gsub("package:", "", search())){
-    if(verbose==TRUE){print("TxDb.Hsapiens.UCSC.hg19.knownGene is loaded correctly")}
-  } else {print("please install and/or load TxDb.Hsapiens.UCSC.hg19.knownGene")}
+  if(alignment=="hg19"){
+    if("TxDb.Hsapiens.UCSC.hg19.knownGene" %in% gsub("package:", "", search())){
+      if(verbose==TRUE){print("TxDb.Hsapiens.UCSC.hg19.knownGene is loaded correctly")}
+    } else {print("please install and/or load TxDb.Hsapiens.UCSC.hg19.knownGene")}
+    seqinft<-as.data.frame(seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene))
+  }
+  if(alignment=="hg38"){
+    if("TxDb.Hsapiens.UCSC.hg38.knownGene" %in% gsub("package:", "", search())){
+      if(verbose==TRUE){print("TxDb.Hsapiens.UCSC.hg38.knownGene is loaded correctly")}
+    } else {print("please install and/or load TxDb.Hsapiens.UCSC.hg38.knownGene")}
+    seqinft<-as.data.frame(seqinfo(TxDb.Hsapiens.UCSC.hg38.knownGene))
+  }
   
   #create seqinfo object for all chromosomes to get lengths
-  seqinft19<-as.data.frame(seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene))
-  seqinf19<-as(seqinft19[nchar(rownames(seqinft19))<6,],"Seqinfo")
+  seqinf<-as(seqinft[nchar(rownames(seqinft))<6,],"Seqinfo")
   # delete old chain file if file with this name already exists
   system(paste0("test -e ", out_chain_name, " && rm ", out_chain_name))
   gtf<-import(input_gtf)
   gtf<-gtf %>% filter(type == "exon") #since only care about exome
   gtf_transcripts<-gtf[(elementMetadata(gtf)[,"transcript_id"] %in% transcript_list)]
   if(verbose==TRUE){print("annotation data finished loading")}
-  for(chr in seqinf19@seqnames){
+  for(chr in seqinf@seqnames){
     for(str in c("-", "+")){
       if(verbose==TRUE){print(paste("Chromosome", chr, "strand", str, "starting"))}
       gtf_hold<-gtf_transcripts %>% filter(strand==str, seqnames==chr)
@@ -55,7 +64,7 @@ GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list, 
         gtf_t<-gtf_hold[(elementMetadata(gtf_hold)[,"transcript_id"] == t)]
         length_t<-sum(gtf_t@ranges@width)
         end_position<-start_position+length_t-1
-        first_line<-c(paste0("chain 42 ", chr, " ", seqinf19@seqlengths[which(seqinf19@seqnames==chr)], 
+        first_line<-c(paste0("chain 42 ", chr, " ", seqinf@seqlengths[which(seqinf@seqnames==chr)], 
                              " ", str, " ", gtf_t@ranges@start[1], " ", 
                              gtf_t@ranges@start[length(gtf_t@ranges@start)]+gtf_t@ranges@width[length(gtf_t@ranges@width)]-1,
                              " ", chr, "_", sign, "_", chrom_suffix, " ", length_chr, " ", str, " ", 
