6618810a9ab9d1d6acdab2d74bcd0246e7fabce3
Veronica Busa <veronica.busa@gmail.com>
2019-10-20 21:43:23 -0400

    edit comments for clarity

--- liftOverToExomeBigWig.R
+++ liftOverToExomeBigWig.R
@@ -1,4 +1,4 @@
-#' Liftover strand-separated BigWigs to an exome BigWig
+#' Liftover whole genome strand-separated BigWigs to an exome BigWig
 #' 
 #' `liftOverToExomeBigWig` writes a BigWig exome file that is lifted over from two strand-separated BigWig files.
 #' 
@@ -15,7 +15,7 @@
 #' "CLIP_exome.bw",
 #' "chain.chr.size")
 #' 
-#' #' liftOverToExomeBigWig(c("CLIP_rep1_F.bw", "CLIP_rep1_R.bw"),
+# liftOverToExomeBigWig(c("CLIP_rep1_F.bw", "CLIP_rep1_R.bw"),
 #' "hg19_exome.chain",
 #' "CLIP_exome.bw",
 #' write_chr=F)
@@ -28,6 +28,7 @@ liftOverToExomeBigWig<-function(input_bw, chain, output_bw, write_chr = TRUE, ou
   if("plyranges" %in% gsub("package:", "", search()) == F){print("please install and/or load plyranges")}
   if("dplyr" %in% gsub("package:", "", search()) == F){print("please install and/or load dplyr")}
 
+  #allow flexible naming systems for f and r bw files
   if(length(input_bw)==2){
     f_bw<-input_bw[1]
     r_bw<-input_bw[2]
@@ -44,17 +45,17 @@ liftOverToExomeBigWig<-function(input_bw, chain, output_bw, write_chr = TRUE, ou
   
   chain_object<-import(chain, exclude="junk")
   
+  # Perform liftOver for + and - strands given an exome chain object; reduce to only relevant intervals
+  # and combine into one bw
   liftOver_plus<-liftOver(plus, chain_object)
   # remove any blank intervals (shouldn't be any >1)
   liftOver_plus<-liftOver_plus[(elementNROWS(range(liftOver_plus))==1L)] %>% unlist()
   liftOver_plus<-liftOver_plus[lapply(as.character(liftOver_plus@seqnames),
                                                           function(x){grepl("plus",x) == T}) %>% unlist()]
   liftOver_minus<-liftOver(minus, chain_object)
-  # remove any blank intervals (shouldn't be any >1)
   liftOver_minus<-liftOver_minus[(elementNROWS(range(liftOver_minus))==1L)] %>% unlist()
   liftOver_minus<-liftOver_minus[lapply(as.character(liftOver_minus@seqnames),
                                                             function(x){grepl("minus",x) == T}) %>% unlist()]
-  
   liftOver<-bind_ranges(liftOver_minus, liftOver_plus)
   
   #must re-introduce chromosome lengths into seqinfo
@@ -73,15 +74,14 @@ liftOverToExomeBigWig<-function(input_bw, chain, output_bw, write_chr = TRUE, ou
   }
   seq.info<-do.call(rbind.data.frame, lapply(lines, function(x){strsplit(x, " ") %>% unlist()}))[,8:9]
   colnames(seq.info)<-c("chr", "length")
-  if(write_chr==T){
-    #save seq.length file
-    write.table(seq.info, out_chr, sep="\t", col.names = F, row.names = F, quote = F)
-  }
+  #save seq.length file
+  if(write_chr==T){write.table(seq.info, out_chr, sep="\t", col.names = F, row.names = F, quote = F)}
   liftOver@seqinfo@seqlengths<-seq.info[,2] %>%
     as.character() %>% as.integer()
   liftOver@elementMetadata$score<-1:length(liftOver) #arbitrary score
   liftOver<-liftOver[countOverlaps(liftOver,liftOver) <= 1L] # remove duplicates
   
+  # output exome bw file
   rtracklayer::export(liftOver, con= output_bw)
 }
 
