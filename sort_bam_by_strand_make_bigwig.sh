bcadb4f516c459365aa6c53a45bbf6751100d9bd
Veronica Busa <veronica.busa@gmail.com>
2019-10-31 11:41:57 -0400

    added argument -g genome to get correct chromosome lengths (hg19 or hg38) for bam to bw conversion

--- sort_bam_by_strand_make_bigwig.sh
+++ sort_bam_by_strand_make_bigwig.sh
@@ -3,25 +3,27 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -i input_bam -p prefix_output"
+   echo "Usage: $0 -i input_bam -p prefix_output -g genome"
    echo -e "\t-i Name of inported BAM file to be turned into strand-separated BAM and BigWig files"
    echo -e "\t-p Prefix of output BAM and BigWig files"
+   echo -e "\t-g Alignment used, either hg19 or hg38"
    exit 1 # Exit script after printing help
 }
 
-while getopts "i:p:" opt
+while getopts "i:p:g:" opt
 do
    case "$opt" in
       i ) input_bam="$OPTARG" ;;
       p ) prefix_output="$OPTARG" ;;
+      g ) genome="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$prefix_output" ] || [ -z "$input_bam" ]
+if [ -z "$prefix_output" ] || [ -z "$input_bam" ] || [ -g "$genome" ]
 then
-   echo "-i input_bam and -p prefix_output argumetns are required. Some or all of the parameters are empty";
+   echo "-i input_bam, -g genome alignment, and -p prefix_output arguments are required. Some or all of the parameters are empty";
    helpFunction
 fi
 
@@ -43,6 +45,6 @@ rm reverse.tmp
 
 rm header.tmp
 
-python bam_to_bigwig_edit.py -i "${prefix_output}_forward.bam" -g hg19 -o "${prefix_output}_forward.bw"
+python bam_to_bigwig_edit.py -i "${prefix_output}_forward.bam" -g $genome -o "${prefix_output}_forward.bw"
 
-python bam_to_bigwig_edit.py -i "${prefix_output}_reverse.bam" -g hg19 -o "${prefix_output}_reverse.bw"
+python bam_to_bigwig_edit.py -i "${prefix_output}_reverse.bam" -g $genome -o "${prefix_output}_reverse.bw"
