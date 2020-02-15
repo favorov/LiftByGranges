c1db1f80bbdb9ea7999c80e0bed7facc40e9ccbe
vbusa1 <vbusa1@jhmi.edu>
2020-02-15 13:23:20 -0500

    goes chromosome by chromosome to make one genome rather than transcript by transcript to make each chromosome

--- make_chain_file.R
+++ make_chain_file.R
@@ -2,9 +2,9 @@
 #' 
 #' `GRangesMappingToChainFile` writes a chain file of the exome mapped from a genome annotation file.
 #' 
-#' @param input_gtf The name of the gtf/gff file that will be converted to an exome chain file. Required
+#' @param input_GRange The name of the gtf/gff file that will be converted to an exome chain file. Required
 #' @param out_chain_name The name of the chain file to be created. Required
-#' @param chrom_suffix The suffix to be appended to all chromosome names created in the chain file. Default "exome"
+#' @param chrom_suffix The suffix to be appended to all chromosome names created in the chain file, if desired.
 #' @param verbose Output updates while the function is running. Default FALSE
 #' @param transcript_list a vector of transcript names that represent the most expressed isoform of their respective genes and correspond to gtf annotation names. Required
 #' @param alignment The human genome alignment used, either "hg19" or "hg38". Default "hg19"
@@ -19,16 +19,22 @@
 #' }
 #' 
 
-GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list,
-                                    chrom_suffix = "exome", verbose=FALSE,
-                                    alignment="hg19"){
-  # first load necessary packages, rtracklayer, plyranges, and TxDb.Hsapiens.UCSC.hg19.knownGene
+GRangesMappingToChainFile<-function(input_GRanges,
+                                    chrom_suffix = "",
+                                    verbose=FALSE,
+                                    alignment="hg38"){
+  # first load necessary packages, rtracklayer, plyranges, and TxDb.Hsapiens.UCSC.hg19.knownGene or TxDb.Hsapiens.UCSC.hg38.knownGene
   if("rtracklayer" %in% gsub("package:", "", search())){
     if(verbose==TRUE){print("rtracklayer is loaded correctly")}
   } else {return("please install and/or load rtracklayer")}
   if("plyranges" %in% gsub("package:", "", search())){
     if(verbose==TRUE){print("plyranges is loaded correctly")}
   } else {return("please install and/or load plyranges")}
+  
+  # get alignment info, make sure it is compatible
+  if(!(alignment %in% c("hg19", "hg38"))){
+    return("valid genome alignments are hg19 and hg38")
+  }
   if(alignment=="hg19"){
     if("TxDb.Hsapiens.UCSC.hg19.knownGene" %in% gsub("package:", "", search())){
       if(verbose==TRUE){print("TxDb.Hsapiens.UCSC.hg19.knownGene is loaded correctly")}
@@ -44,62 +50,52 @@ GRangesMappingToChainFile<-function(input_gtf, out_chain_name, transcript_list,
   
   #create seqinfo object for all chromosomes to get lengths
   seqinf<-as(seqinft[nchar(rownames(seqinft))<6,],"Seqinfo")
-  # delete old chain file if file with this name already exists
-  system(paste0("test -e ", out_chain_name, " && rm ", out_chain_name))
-  gtf<-import(input_gtf)
-  gtf<-gtf %>% filter(type == "exon") #since only care about exome
-  gtf_transcripts<-gtf[(elementMetadata(gtf)[,"transcript_id"] %in% transcript_list)]
-  if(verbose==TRUE){print("annotation data finished loading")}
   # write chain file chromosome-by-chromosome
-  if(sum(gtf_transcripts@seqnames %in% seqinf@seqnames) == 0){
-    gtf_transcripts@seqnames<-paste0("chr", gtf_transcripts@seqnames) %>% Rle()
-    if(sum(gtf_transcripts@seqnames %in% seqinf@seqnames) == 0){
+  if(sum(input_GRanges@seqnames %in% seqinf@seqnames) == 0){
+    input_GRanges@seqnames<-paste0("chr", input_GRanges@seqnames) %>% Rle()
+    if(sum(input_GRanges@seqnames %in% seqinf@seqnames) == 0){
       return("GTF chromosomes do not resemble UCSC chromosome names. Suggested format: 'chr#' or just # for chromosome name, e.g. chr1 chr10 chrM")
     }
   }
+  
+  row<-1
+  interval<-1
+  chrom_chains<-matrix(NA, ncol=3, nrow=(length(input_GRanges)))
+  
+  # make a chain for each chromosome in the genome
   for(chr in seqinf@seqnames){
-    for(str in c("-", "+")){
-      if(verbose==TRUE){print(paste("Chromosome", chr, "strand", str, "starting"))}
-      gtf_hold<-gtf_transcripts %>% filter(strand==str, seqnames==chr)
-      if(length(gtf_hold@ranges)==0){next} # in case of chromosomes without data on one strand
-      length_chr<-sum(gtf_hold@ranges@width)
-      if(str=="+"){sign<-"plus"}else{sign<-"minus"}
-      transcripts<- unique(gtf_hold@elementMetadata@listData[["transcript_id"]])
-      chrom_chains<-matrix(NA, ncol=3, nrow=(length(gtf_hold)+(2*length(transcripts))))
-      row<-1
-      start_position=1
-      # make a chain for each transcript in the chromosome
-      for(t in transcripts){
-        gtf_t<-gtf_hold[(elementMetadata(gtf_hold)[,"transcript_id"] == t)]
-        length_t<-sum(gtf_t@ranges@width)
-        end_position<-start_position+length_t-1
-        first_line<-c(paste0("chain 42 ", chr, " ", seqinf@seqlengths[which(seqinf@seqnames==chr)], 
-                             " ", str, " ", gtf_t@ranges@start[1], " ", 
-                             gtf_t@ranges@start[length(gtf_t@ranges@start)]+gtf_t@ranges@width[length(gtf_t@ranges@width)]-1,
-                             " ", chr, "_", sign, "_", chrom_suffix, " ", length_chr, " ", str, " ", 
-                             start_position, " ", end_position," ", t), "", "")
-        txpt<-matrix(NA, nrow=length(gtf_t@ranges@start), ncol=3)
-        txpt[,1]<-gtf_t@ranges@width
-        if(length(gtf_t@ranges@width)>1){ #for cases where there is only 1 exon in a transcript
-          txpt[,2]<-c(gtf_t@ranges@start[2:length(gtf_t@ranges@start)]-
-                        (gtf_t@ranges@width[1:(length(gtf_t@ranges@width)-1)]+
-                         gtf_t@ranges@start[1:(length(gtf_t@ranges@width)-1)])-1, "")
-          txpt[,3]<-0
-          txpt[nrow(txpt),3]<-""
-        }
-        chrom_chains[row,]<-first_line
-        chrom_chains[(row+1):(row+nrow(txpt)),]<-txpt
-        chrom_chains[row+nrow(txpt)+1,]<-c("", "", "")
-        start_position<-end_position
-        row<-row+nrow(txpt)+2
-      }
-      # append the chains for the chromosome to the chain file
-      write.table(chrom_chains, file=out_chain_name, append=T, sep="\t", row.names = F, col.names = F, quote = F, na="")
-      if(verbose==TRUE){print(paste("Chromosome", chr, "strand", str, "complete"))}
-    }
+    if(verbose==TRUE){print(paste("Chromosome", chr, "starting"))}
+    gtf_hold<-input_GRanges %>% filter(seqnames==chr)
+    if(length(gtf_hold@ranges)==0){next} # in case of chromosomes without data
+    length_chr<-sum(gtf_hold@ranges@width)
+    first_line<-c(paste0("chain 42 ", chr, " ", seqinf@seqlengths[which(seqinf@seqnames==chr)], 
+                         " * ", gtf_hold@ranges@start[1], " ", 
+                         gtf_hold@ranges@start[length(gtf_hold@ranges@start)]+gtf_hold@ranges@width[length(gtf_hold@ranges@width)]-1,
+                         " ", chr, chrom_suffix, " ", length_chr, " * 1 ", 
+                         length_chr," ", interval), "", "")
+    interval<-interval + 1
+    chrom<-matrix(NA, nrow=length(gtf_hold@ranges@start), ncol=3)
+    chrom[,1]<-gtf_hold@ranges@width
+    if(length(gtf_hold@ranges@width)>1){ #for cases where there is only 1 interval in a chromosome
+      chrom[,2]<-c(gtf_hold@ranges@start[2:length(gtf_hold@ranges@start)]-
+                  (gtf_hold@ranges@width[1:(length(gtf_hold@ranges@width)-1)]+
+                  gtf_hold@ranges@start[1:(length(gtf_hold@ranges@width)-1)])-1, "")
+      chrom[,3]<-0
+      chrom[nrow(chrom),3]<-""
+      } 
+    chrom_chains[row,]<-first_line
+    chrom_chains[(row+1):(row+nrow(chrom)),]<-chrom
+    chrom_chains[row+nrow(chrom)+1,]<-c("", "", "")
+    row<-row+nrow(chrom)+2
   }
+  chrom_chains<-
+  test<-as.character(chr)
+    # append the chains for the chromosome to the chain file
+    #write.table(chrom_chains, file=out_chain_name, append=T, sep="\t", row.names = F, col.names = F, quote = F, na="")
+    #if(verbose==TRUE){print(paste("Chromosome", chr, "complete"))}
+
   #this is a band-aid fix since import cannot differentiate chains without a truly blank line between chains
-  remove_blanks<-readLines(out_chain_name)
-  remove_blanks<-gsub("\t\t", "", remove_blanks)
-  write(remove_blanks, file=out_chain_name, sep="")
+  #remove_blanks<-readLines(out_chain_name)
+  #remove_blanks<-gsub("\t\t", "", remove_blanks)
+  #write(remove_blanks, file=out_chain_name, sep="")
 }
